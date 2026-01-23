d1a3b3c43107b3ff56a064bb0849f410fe715e24
Matheus Lima <matheuslima@duck>
2026-01-22 22:54:20 -0300

    Adiciona DbSet para AutorModel e LivroModel no AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
 
 namespace WebApi.Data
 {
@@ -11,7 +12,8 @@ namespace WebApi.Data
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
-            
         }
-    }
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
+        }
 }
