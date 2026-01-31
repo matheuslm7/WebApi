7cb4d049551696cefa1b6d2081ae8d1a9049d28d
Matheus Lima <matheuslima@duck>
2026-01-31 17:40:04 -0300

    fix: Atualizar versão informacional e arquivos de saída do projeto

--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb33e2f39672c9ee5d6057fd260ca482cea6647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24bab3b24680ec0bb49a224f58b8e4cc76c64973")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
